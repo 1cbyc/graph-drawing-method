026d6cb85a10b8fa299a38f5388650c7311e0e81
1cbyc <isaacnsisong@gmail.com>
2023-10-31 22:30:22 +0100

    Updated Basic Web View

---
+++ web-view/plot-graph.php
@@ -0,0 +1,35 @@
+<?php
+const DMS = 17;
+
+function plot($m, $c, &$arr) {
+    for ($x = -DMS; $x <= DMS; $x++) {
+        if (($m * $x + $c + DMS / 2) < DMS && ($m * $x + $c + DMS / 2) >= 0 && ($x * 2 + DMS) < DMS * 2 + 1 && ($x * 2 + DMS) >= 0) {
+            $arr[($m * $x + $c + DMS / 2)][$x * 2 + DMS] = 'x';
+        }
+    }
+}
+
+$m = $_POST['m'];
+$c = $_POST['c'];
+
+if (!is_numeric($m) || !is_numeric($c)) {
+    echo "Invalid input. Please enter numeric values for m and c.";
+    exit(1);
+}
+
+$arr = array_fill(0, DMS, array_fill(0, DMS * 2 + 1, ' '));
+
+for ($i = 0; $i < DMS; $i++) {
+    for ($j = 0; $j < DMS * 2 + 1; $j++) {
+        $arr[$i][$j] = ($i == DMS / 2 ? '-' : ($j == DMS ? chr(197) : ' '));
+    }
+}
+
+$arr[DMS / 2][DMS] = chr(197);
+
+plot($m, $c, $arr);
+
+for ($i = 0; $i < DMS; $i++) {
+    echo implode('', $arr[$i]) . "<br>";
+}
+?>
