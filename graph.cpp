c71f57f062e72ecc6d8c9722c8b3228933ec0ec5
1cbyc <isaacnsisong@gmail.com>
2023-10-31 22:14:01 +0100

    i just made the 2023 update

---
+++ graph.cpp
@@ -0,0 +1,51 @@
+// I uypda
+
+#include <iostream>
+#include <string>
+const int DMS = 17;
+
+// Function to plot a straight line graph
+void plot(int m, int c, std::string arr[DMS][DMS * 2 + 1]) {
+    for (int x = -DMS; x <= DMS; x++) {
+        if ((-(m * x + c) + DMS / 2) < DMS && (-(m * x + c) + DMS / 2) >= 0 && (x * 2 + DMS) < DMS * 2 + 1 && (x * 2 + DMS) >= 0) {
+            arr[(-(m * x + c) + DMS / 2)][x * 2 + DMS] = "x";
+        }
+    }
+}
+
+int main() {
+    try {
+        std::string arr[DMS][DMS * 2 + 1];
+        int m, c;
+        
+        std::cout << "Enter the gradient (m): ";
+        std::cin >> m;
+
+        std::cout << "Enter the y-intercept (c): ";
+        std::cin >> c;
+
+        if (std::cin.fail()) throw 1;
+
+        // Initialize the array
+        for (int i = 0; i < DMS; i++) {
+            for (int j = 0; j < DMS * 2 + 1; j++) {
+                arr[i][j] = (i == DMS / 2 ? std::string(1, '-') : (j == DMS ? std::string(1, char(197)) : " "));
+            }
+        }
+
+        arr[DMS / 2][DMS] = std::string(1, char(197));
+
+        plot(m, c, arr);
+
+        // Print the graph
+        for (int i = 0; i < DMS; i++, std::cout << "\n") {
+            for (int j = 0; j < DMS * 2 + 1; j++) {
+                std::cout << arr[i][j];
+            }
+        }
+
+        return 0;
+    } catch (int err) {
+        std::cout << "Invalid input. Please try again.";
+    }
+}
